80aacb51c4f735c52478c0f6ab6115c3035dc504
lumicarvalho <lumaecarvalho@gmail.com>
2022-12-11 12:20:17 -0300

    Projeto realizado através das aulas do Bootcamp da DIO

---
+++ calculadora-dio/src/components/Input/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #DFE9F5;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+    
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #7C6EA7;
+        border: 2px solid #CDCDCD;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        padding: 0 10px;
+        font-size: 24px;
+        font-family: 'Roboto';
+        color: #FFFFFF;
+    }
+`
